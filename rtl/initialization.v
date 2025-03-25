4f4ff00c5bd504ecc4441627536b8d183c52c47e
Karthik <karthikmble@gmail.com>
2025-03-25 14:03:50 -0500

    fixed mbit data storing to textbook

--- rtl/initialization.v
+++ rtl/initialization.v
@@ -18,28 +18,30 @@ module initialization(
 	always @(posedge clk or posedge rst) begin
 		if (rst) begin
 			mbit_r <= 'b0;
-		end else if (icount >= 'd0 && icount <= 'd127) begin
-			mbit_r[icount] <= key_in[icount];
-		end else if (icount > 'd127 && icount <= 'd255) begin
-			mbit_r[icount] <= key_in[icount - 'd128];
-		end else if (icount == 'd256) begin
-			mbit_r[icount] <= key_in[0] ^ 1;
-		end else if (icount > 'd256 && icount <= 'd1535) begin
-			mbit_r[icount] <= key_in[(icount - 'd256) % 'd128];
+		end else if (icount >= 'd1665 && icount <= 'd1792) begin
+			mbit_r[icount] <= key_in['d1792 - icount];
+		end else if (icount >= 'd1537 && icount <= 'd1664) begin
+			mbit_r[icount] <= iv_in['d1664 - icount];
+		end else if (icount == 'd1536) begin
+			// mbit_r[icount] <= key_in[0] ^ 1;
+			mbit_r[icount] <= 'b0;
+		end else if (icount >= 'd1 && icount <= 'd1535) begin
+			mbit_r[icount] <= key_in[icount % 'd128];
 		end else begin
 			mbit_r[icount] <= 'b0;
 		end
 	end
 
 	always @(posedge clk or posedge rst) begin
-		if (rst | icount == 'd1793) begin
-			icount <= 'b0;
+		if (rst | icount == 'd0) begin
+			icount <= 'd1792;
 			state_r <= 'b0;
 		end else if (start_ipi) begin
-			icount <= icount + 1'b1;
+			icount <= icount - 1'b1;
 			state_r <= state_ur;
 		end else begin
 			icount <= 'b0;
+			mbit_r <= 'b0;
 		end
 	end
 
@@ -54,12 +56,12 @@ module initialization(
 		.ca_in(1'b1),
 		.cb_in(1'b1),
 		.state_io(state_r),
-		.mbit_in(mbit_sw),
+		.mbit_in(mbit_r[icount]),
 		.sup128_out(state_ur)
 	);
 
 	assign state_out 	= state_r;
 	assign mbit_out 	= mbit_r;
-	assign mbit_sw 	= (icount > 'b0) ? mbit_r[icount-1'b1] : mbit_r[icount];
+	// assign mbit_sw 	= (icount > 'b0) ? mbit_r[icount] : mbit_r[icount];
 
 endmodule
--- rtl/ksg128.v
+++ rtl/ksg128.v
@@ -1,7 +1,8 @@
 module ksg128(
 	input [292:0] state_in,
 	output ks_out
-);
+	);
+
 	wire maj_out, ch_out;
 
 	maj_fn MAJ(state_in[235], state_in[61], state_in[193], maj_out);
