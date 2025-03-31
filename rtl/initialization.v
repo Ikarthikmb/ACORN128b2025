480b45364bff7b4178ea16cfb0bf1d3852a42aec
Karthik <karthikmble@gmail.com>
2025-03-31 16:04:18 -0500

    M changed from reg to wire

--- rtl/initialization.v
+++ rtl/initialization.v
@@ -11,8 +11,8 @@ module initialization(
 	output	[1792:0] mbit_out,
 	output	[292:0] state_out
 );
+
 	reg [11:0] icount;
-	reg [1792:0] mbit_r;
 	reg [292:0] state_pstr;
 	reg done_ipr;
 
@@ -20,28 +20,8 @@ module initialization(
 	wire mbit_sw;
 
 	always @(posedge clk or posedge rst) begin
-		if (rst | ~start_ipi) begin
-			mbit_r <= 'b0;
-			done_ipr <= 'b0;
-		end else if (start_ipi & icount >= 'd1665 & icount <= 'd1792) begin
-			mbit_r[icount] <= key_in['d1792 - icount];
-		end else if (start_ipi & icount >= 'd1537 & icount <= 'd1664) begin
-			mbit_r[icount] <= iv_in['d1664 - icount];
-		end else if (start_ipi & icount == 'd1536) begin
-			// mbit_r[icount] <= key_in[0] ^ 1;
-			mbit_r[icount] <= 'b0;
-		end else if (start_ipi & icount >= 'd1 & icount <= 'd1535) begin
-			mbit_r[icount] <= key_in[icount % 'd128];
-		end else if (icount == 'b0) begin
-			done_ipr <= 'b1;
-		end else begin
-			mbit_r[icount] <= 'b0;
-		end
-	end
-
-	always @(posedge clk or posedge rst) begin
 		if (rst | icount == 'd0) begin
-			icount <= 'd1792;
+			icount <= 'd1793;
 			state_pstr <= 'b0;
 		end else if (start_ipi) begin
 			icount <= icount - 1'b1;
@@ -50,9 +30,8 @@ module initialization(
 	end
 
 	always @(posedge start_ipi) begin
-		icount <= 'd1792;
+		icount <= 'd1793;
 		state_pstr <= 'b0;
-		mbit_r <= 'b0;
 		done_ipr <= 'b0;
 	end
 
@@ -66,10 +45,24 @@ module initialization(
 		.sup128_out(state_nxtw)
 	);
 
-	assign state_out 	= state_pstr;
-	assign mbit_out 	= mbit_r;
-	// assign mbit_sw 	= ( (icount > 1'b0) & (icount < 'd1792) ) ? mbit_r[icount+1'b1] : 1'b0;
-	assign mbit_sw 	= ((icount > 0) & (icount < 'd1792)) ? mbit_r[icount] : 1'b0;
-	assign done_ipo = done_ipr;
+	assign state_out	= state_pstr;
+	assign mbit_sw 		= (icount > 0 & icount <= 1792) ? mbit_out[icount] : 1'b0;
+	assign done_ipo 	= done_ipr;
 
+	assign mbit_out[1792:1665] 	= key_in[127:0];
+	assign mbit_out[1664:1537] 	= iv_in[127:0];
+	assign mbit_out[1536] 		= key_in[0] ^ 1;
+	assign mbit_out[1535:1409] 	= key_in[127:0];
+	assign mbit_out[1408:1281] 	= key_in[127:0];
+	assign mbit_out[1280:1153] 	= key_in[127:0];
+	assign mbit_out[1152:1025] 	= key_in[127:0];
+	assign mbit_out[1024:897] 	= key_in[127:0];
+	assign mbit_out[896:769]	= key_in[127:0];
+	assign mbit_out[768:641]	= key_in[127:0];
+	assign mbit_out[640:513]	= key_in[127:0];
+	assign mbit_out[512:385]	= key_in[127:0];
+	assign mbit_out[384:257]	= key_in[127:0];
+	assign mbit_out[256:129]	= key_in[127:0];
+	assign mbit_out[128:1] 		= key_in[127:0];
+	assign mbit_out[0] = 'b0;
 endmodule
