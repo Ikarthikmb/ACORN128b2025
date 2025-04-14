1999eac7170f855010ccdbbb30ed00f2b2c2b54c
Karthik <karthikmble@gmail.com>
2025-04-14 13:16:12 -0500

    state, fb functions added in top; correceted according to the top/state in rtl & tb

--- rtl/acorn128_top.v
+++ rtl/acorn128_top.v
@@ -1,4 +1,3 @@
-`define DEBUG
 
 module acorn128_top(
     input clk,
@@ -17,172 +16,207 @@ module acorn128_top(
     output ready_out
 );
 
-	reg	start_ipr;
-	reg start_ppr;
-	reg start_epr;
-	reg	start_fpr;
-	wire done_ipw;
-	wire done_ppw;
-	wire done_epw;
-	wire done_fpw;
+	wire ks_outw;
+	wire fbk_outw;
+    reg	[127:0]	plaintext_r;
+	reg [11:0] counter_r;
+	reg [11:0] counter_er;
 	reg ready_r;
-    reg [127:0]	keystream_r;
-    reg [127:0]	plaintext_r;
-    reg [63:0]	counter_r;
-    reg [7:0]	phase_r;
-
-    wire [127:0]	ciphertext_w;
-    wire [127:0]	tag;
-    wire [292:0]	state_aw;
-    wire [292:0]	state_iw;
-	wire [1792:0]	mbit_iow;
-	wire [1792:0]	mbit_aow;
-	wire [1792:0]	mbit_eow;
+	reg [3:0] phase_r;
+	reg [292:0] state_pstr;
+	reg [292:0] state_nxtr;
+	reg ca_state_ir;
+	reg cb_state_ir;
+	reg mbit_state_ir;
+	reg [127:0] cipher_r;
+	reg [127:0] tag_r;
+	reg start_sp;
+	wire ca_ip_ow, ca_ap_ow, ca_ep_ow;
+	wire cb_ip_ow, cb_ap_ow, cb_ep_ow;
+	wire mbit_ip_ow, mbit_ap_ow, mbit_ep_ow;
 
     localparam WAITING_P		= 'd0;
     localparam INITIALIZATION_P	= 'd1;
-    localparam PROCESSING_P		= 'd2;
+    localparam ASSOCIATED_P		= 'd2;
     localparam ENCRYPTION_P		= 'd3;
     localparam FINAL_P			= 'd4;
 
     always @(posedge clk or posedge rst) begin
         if (rst) begin
-            keystream_r 	<= 128'b0;
-            plaintext_r 	<= 128'b0;
-            counter_r 		<= 'd1792;
-            ready_r 		<= 'b0;
-            phase_r 		<= WAITING_P;
-			start_ipr 		<= 'b0;
-			start_ppr 		<= 'b0;
-			start_epr 		<= 'b0;
-			start_fpr 		<= 'b0;
+            state_pstr <= 'b0;
+		end else if(start_sp & (phase_r >= 1'b1) & (counter_r <= 'd1792) ) begin
+            state_pstr <= state_nxtr;
+		// end else if (start_sp & phase_r > 1'b1) begin
+		end else begin
+            state_pstr <= state_pstr;
+		end
+	end
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            counter_r <= 'b0;
+		end else if(start_sp & phase_r == 1'b1) begin
+            counter_r <= counter_r - 1'b1;
+		end else if(start_sp & phase_r > 1'b1) begin
+            counter_r <= counter_r + 1'b1;
+		end
+	end
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            plaintext_r <= 128'b0;
+            counter_r   <= 'd1792;
+            ready_r     <= 'b0;
+            cipher_r     <= 'b0;
+            phase_r     <= WAITING_P;
+			state_pstr <= 293'b0;
+			start_sp <= 1'b0;
+			ca_state_ir <= 1'b0;
+			cb_state_ir <= 1'b0;
+			mbit_state_ir <= 1'b0;
+			tag_r <= 'b0;
         end else if (start_in) begin
             case (phase_r)
 				WAITING_P: begin
-					if (encrypt_in) begin
-						phase_r <= INITIALIZATION_P;
-						counter_r <= 'd1792;
-						start_ipr <= 'b1;
-					end else begin
-						phase_r <= WAITING_P;
+					phase_r <= INITIALIZATION_P;
+					counter_r <= 'd1792;
+					ca_state_ir <= ca_ip_ow;
+					cb_state_ir <= cb_ip_ow;
+					mbit_state_ir <= mbit_ip_ow;
+					state_pstr <= 293'b0;
+					plaintext_r <= encrypt_in ? plaintext_in : ciphertext_in;
+				end
+
+                INITIALIZATION_P: begin
+					if (counter_r == 'd1792) begin
+						// state_pstr <= 293'b0;
+						start_sp <= 1'b1;
+					end else if (counter_r > 0 & counter_r < 'd1792) begin
+						ca_state_ir <= ca_ip_ow;
+						cb_state_ir <= cb_ip_ow;
+						mbit_state_ir <= mbit_ip_ow;
+					end else if (counter_r == 'b0) begin
+						phase_r <= ASSOCIATED_P;
+						ca_state_ir <= ca_ap_ow;
+						cb_state_ir <= cb_ap_ow;
+						mbit_state_ir <= mbit_ap_ow;
 					end
 				end
-                INITIALIZATION_P: begin
-					if (counter_r > 0) begin 
-						counter_r <= counter_r - 1;
-						// ddisplay($sformatf("Initialization counter: ", counter_r));
-					end else if (counter_r <= 'd1) begin
-						phase_r <= PROCESSING_P;
-						counter_r <= 'd383;
-						start_ipr <= 'b0;
-						start_ppr <= 'b1;
-						if (~done_ipw) begin
-							$display("[ERROR] INITILIZATION INCOMPLETE");
-							if (debug) begin
-								dddisplay("counter: %0d", counter_r);
-								dddisplay("done_ipw: %0b", done_ipw);
-							end
-						end
-					end
-                end
-                PROCESSING_P: begin
-                    if (counter_r > 0) begin
-                        counter_r <= counter_r - 1;
-                    end else if (counter_r <= 'd1) begin
+
+                ASSOCIATED_P: begin
+                    if (counter_r > 0 & counter_r <= 'd382) begin
+						ca_state_ir <= ca_ap_ow;
+						cb_state_ir <= cb_ap_ow;
+						mbit_state_ir <= mbit_ap_ow;
+                    end else if (counter_r == 'd383) begin
+						ca_state_ir <= ca_ap_ow;
+						cb_state_ir <= cb_ap_ow;
+						mbit_state_ir <= mbit_ap_ow;
                         phase_r <= ENCRYPTION_P;
-                        counter_r <= 'd767;
-						start_ppr <= 'b0;
-						start_epr <= 'b1;
                     end
                 end
                 ENCRYPTION_P: begin
-                    if (counter_r > 0) begin
-                        counter_r <= counter_r - 1;
-                    end else if (counter_r <= 'b1) begin
-                        phase_r <= FINAL_P;
-                        counter_r <= 'd1535;
-						start_epr <= 'b0;
-						start_fpr <= 'b1;
-                    end
+                    if (counter_r >= 12'd384 & counter_r <= 12'd511) begin
+						ca_state_ir <= ca_ep_ow;
+						cb_state_ir <= cb_ep_ow;
+						mbit_state_ir <= mbit_ep_ow;
+						cipher_r[counter_r - 12'd384] <= plaintext_in[counter_r - 12'd384] ^ ks_outw;
+					end else if (counter_r > 'd511 & counter_r <= 12'd766) begin
+						ca_state_ir <= ca_ep_ow;
+						cb_state_ir <= cb_ep_ow;
+						mbit_state_ir <= mbit_ep_ow;
+					end else if (counter_r == 12'd767) begin
+						ca_state_ir <= ca_ep_ow;
+						cb_state_ir <= cb_ep_ow;
+						mbit_state_ir <= mbit_ep_ow;
+						phase_r <= FINAL_P;
+					end
                 end
+
                 FINAL_P: begin
-                    if (counter_r > 0) begin
-                        counter_r <= counter_r - 1;
-                    end else if (counter_r <= 'b1) begin
-						start_fpr <= 'b0;
+                    if (counter_r >= 12'd768 & counter_r < 12'd1408) begin
+						ca_state_ir <= 1'b1;
+						cb_state_ir <= 1'b1;
+						mbit_state_ir <= 1'b0;
+                    end else if (counter_r == 12'd1408) begin
+						tag_r <= ks_outw;
+                    end else if (counter_r >= 12'd1409 & counter_r <= 12'd1535) begin
+						tag_r <= tag_r || ks_outw;
+                    end else if (counter_r == 'd1536) begin
                         ready_r <= 'b1;
+                        start_sp <= 'b0;
                     end
                 end
             endcase
-        end
+
+        end else begin
+            plaintext_r <= 128'b0;
+            counter_r 	<= 'd1792;
+            ready_r 	<= 'b0;
+            phase_r 	<= WAITING_P;
+			$display("[INFO] Waiting for START Signal ...");
+		end
     end
+
 
 	initialization INITIALIZATION_M(
 		.clk(clk),
 		.rst(rst),
-		.start_ipi(start_ipr),
+		.count_ip(counter_r),
 		.key_in(key_in),
 		.iv_in(iv_in),
-		.done_ipo(done_ipw),
-		.mbit_out(mbit_iow),
-		.state_out(state_iw)
+		.ca_out(ca_ip_ow),
+		.cb_out(cb_ip_ow),
+		.mbit_out(mbit_ip_ow)
 	);
 
 	associated_process ASSOCIATED_M(
 		.clk(clk),
 		.rst(rst),
-		.start_ppi(start_ppr),
-		.mbit_in(mbit_iow),
-		.state_in(state_iw),
+		.count_ap(counter_r), 
 		.ad_in(associated_data_in),
-		.mbit_out(mbit_aow),
-		.state_out(state_aw)
+		.ca_out(ca_ap_ow),
+		.cb_out(cb_ap_ow),
+		.mbit_out(mbit_ap_ow)
 	);
 
 	encryption ENCRYPTION_M(
 		.clk(clk),
 		.rst(rst),
-		.start_epi(start_epr),
-		.mbit_in(mbit_aow),
-		.state_in(state_aw),
+		.count_ep(counter_r),
 		.plaintext_in(plaintext_in),
-		.mbit_out(mbit_eow),
-		.cipher_out(ciphertext_w)
+		.ca_out(ca_ep_ow),
+		.cb_out(cb_ep_ow),
+		.mbit_out(mbit_ep_ow)
 	);
 
-	finalization FINALIZATION_M(
+	state_update128 STATE_UPDATE128(
 		.clk(clk),
 		.rst(rst),
-		.start_fpi(start_fpr),
-		.mbit_in(mbit_aow),
-		.state_in(state_aw),
-		.tag(tag)
+		.ca_in(ca_state_ir),
+		.cb_in(cb_state_ir),
+		.fbk_in(fbk_outw),
+		.state_in((!start_sp)? state_pstr : state_nxtr),
+		.mbit_in(mbit_state_ir),
+		.sup128_out(state_nxtr)
+	);
+
+	ksg128 KSG128(
+		.state_in(state_pstr),
+		.ks_out(ks_outw)
+	);
+
+	fbk128 FBK128(
+		.state_in((!start_sp)? state_pstr : state_nxtr),
+		.ca_in(ca_state_ir),
+		.cb_in(cb_state_ir),
+		.ks_in(ks_outw),
+		.fout(fbk_outw)
 	);
 
 	assign ready_out = ready_r;
-    assign ciphertext_out = ciphertext_w;
-	assign tag_out = tag;
-
-	task ddisplay(input string text);
-		`ifdef DEBUG
-			$display("[DEBUG] %s",text);
-		`endif
-	endtask
-
-	task dddisplay(input string text, input reg [2000:0] val);
-		`ifdef DEBUG
-			$display("[DEBUG] ",$sformatf(text, val));
-		`endif
-	endtask
-
-	reg debug = 1;
-	initial begin
-		`ifdef DEBUG
-			debug = 1;
-		`else
-			debug = 0;
-		`endif
-	end
+    assign ciphertext_out = cipher_r;
+	assign tag_out = tag_r;
 
 endmodule
 
